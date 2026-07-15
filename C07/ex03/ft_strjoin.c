30d96e391c13e1b270dd0c61b278fe7d916d7cd5
nico <nicolas.parolini@gmail.com>
2026-07-15 23:41:31 +0200

    kzgvigzu

---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cont(char **tab)
+{
+    int res = 0;
+    int i = 0;
+    int x;
+
+    while (tab[i])
+    {
+        x = 0;
+        while (tab[i][x])
+        {
+            res++;
+            x++;
+        }
+        i++;
+    }
+    printf ("%d",res);
+    return (res);
+}
+
+
+char *ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*chain;
+	int	n;
+	
+	n = cont (strs);
+	chain = malloc(sizeof * chain * n);
+	if (chain == NULL)
+		return (0);
+	
+		return (chain);
+}
+
+int	main(int argc, char *argv[])
+{
+	char	*env[] = {"aaa","bbb","ccc","ddd",NULL};
+	char	*result;
+	result = ft_strjoin(5,env,".");
+	return (0);
+}
