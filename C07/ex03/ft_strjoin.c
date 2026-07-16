8dabd51dda33a03916f543fa2912d9d9fdf3167f
Nicolas Parolini <nparolin@c1r14s6.42lausanne.ch>
2026-07-16 11:24:11 +0200

    saw

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int cont(char **tab)
+int cont(char **tab, int taille, char *escp)
 {
-    int res = 0;
-    int i = 0;
-    int x;
+	int res = 0;
+	int i = 0;
+	int x;
 
-    while (tab[i])
-    {
-        x = 0;
-        while (tab[i][x])
-        {
-            res++;
-            x++;
-        }
-        i++;
-    }
-    printf ("%d",res);
-    return (res);
+	while (tab[i])
+	{
+	x = 0;
+	while (tab[i][x])
+	{
+		res++;
+		x++;
+	}
+	i++;
+	}
+	i = 0;
+	while (escp[i])
+	{
+		i++;
+	}
+	i--;
+	i = i * taille;
+	res = res + i;
+	printf ("%d",res);
+	return (res);
 }
 
 
@@ -27,7 +35,7 @@ char *ft_strjoin(int size, char **strs, char *sep)
 	char	*chain;
 	int	n;
 	
-	n = cont (strs);
+	n = cont (strs, size, sep);
 	chain = malloc(sizeof * chain * n);
 	if (chain == NULL)
 		return (0);
